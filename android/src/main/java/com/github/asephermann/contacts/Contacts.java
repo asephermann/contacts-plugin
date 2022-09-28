9044c9bfd6d1352d619e0b25787edff40c0db774
Asep Herman Nursalam <asephnursalam@gmail.com>
2022-09-28 16:24:50 +0700

    Initial commit

---
+++ android/src/main/java/com/github/asephermann/contacts/Contacts.java
@@ -0,0 +1,11 @@
+package com.github.asephermann.contacts;
+
+import android.util.Log;
+
+public class Contacts {
+
+    public String echo(String value) {
+        Log.i("Echo", value);
+        return value;
+    }
+}
